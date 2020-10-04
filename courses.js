f7d6996ae169ce9161ac996e138dcad9d62b63bd
Michael Kofron <kofronmichael@gmail.com>
2020-10-04 14:24:50 -0500

    setup holes

--- courses.js
+++ courses.js
@@ -1,3 +1,43 @@
 document.getElementById("create-course").addEventListener("click", function(){
     document.getElementById("create-course-box").style.display = "flex"
 })
+
+document.getElementById("setup-holes").addEventListener("click", function(){
+    let holeCount = parseInt(document.getElementById("course-hole-count").value)
+
+    let parent = document.getElementById("create-course-box")
+
+    for (let i = 1; i <= holeCount; i++){
+        let holeNumberInput = document.createElement("input")
+        holeNumberInput.setAttribute("class", "hole-number")
+        holeNumberInput.setAttribute("placeholder", "hole number")
+        holeNumberInput.value = i
+
+        parent.appendChild(holeNumberInput)
+
+        let holeNameInput = document.createElement("input")
+        holeNameInput.setAttribute("class", "hole-name")
+        holeNameInput.setAttribute("placeholder", "hole name")
+
+        parent.appendChild(holeNameInput)
+
+        let holeParInput = document.createElement("input")
+        holeParInput.setAttribute("class", "hole-par")
+        holeParInput.setAttribute("placeholder", "hole par")
+
+        parent.appendChild(holeParInput)
+
+        let mensHandicapInput = document.createElement("input")
+        mensHandicapInput.setAttribute("class", "hole-mens-handicap")
+        mensHandicapInput.setAttribute("placeholder", "men's handicap")
+
+        parent.appendChild(mensHandicapInput)
+
+        let womensHandicapInput = document.createElement("input")
+        womensHandicapInput.setAttribute("class", "hole-womens-handicap")
+        womensHandicapInput.setAttribute("placeholder", "women's handicap")
+
+        parent.appendChild(womensHandicapInput)
+
+    }
+})
