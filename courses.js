80fa214490852dc8005d82d30d6f594e40b6e718
Michael Kofron <kofronmichael@gmail.com>
2020-10-05 17:20:29 -0500

    raw current hole

--- courses.js
+++ courses.js
@@ -254,7 +254,9 @@ let playCourse = {
 
 }
 
-let currentHoleId
+let currentHoleIndex = 0
+
+let rawCurrentHole = playCourse.holes[currentHoleIndex]
 
 let round = {
 
@@ -330,7 +332,6 @@ function startRound(course){
     parent.innerHTML = `<p id='course-title'>Title: ${course.name}</p><p id='holes'>Holes: ${course.holes.length}</p><p id='current-hole'>Current Hole: 1</p><p id='current-shot'>Current Shot: 0</p>`
 
     round.currentHole = 1
-    currentHoleId = playCourse.holes[0].id
     round.currentShot = 0
     round.userKey = key
 
@@ -365,7 +366,7 @@ function startRound(course){
     finishHole.addEventListener("click", function(){
         if (round.currentHole < playCourse.holes.length){
             round.currentHole++
-            currentHoleId++
+            currentHoleIndex++
         }
 
         document.getElementById("current-hole").innerText = `Current Hole: ${round.currentHole}`
