2fbcbe3b50c895b28a8a885b92c59e146aa2ed58
Michael Kofron <kofronmichael@gmail.com>
2020-10-05 16:07:14 -0500

    phone test 2

--- courses.js
+++ courses.js
@@ -384,8 +384,11 @@ function takeShot(callback){
                 clearInterval(interval)
                 infoArea.innerHTML = `<p>Lat: ${currentShot.latitude}, Long: ${currentShot.longitude}</p><input id='club' type='text' placeholder='club selection'><button id='submit-shot'>submit shot</button>`
                 document.getElementById('submit-shot').addEventListener("click", function(){
-                    document.getElementById("silent").style.backgroundColor = "red"
                     currentShot.clubSelection = document.getElementById("club-selection").value
+
+                    if (currentShot.clubSelection == "Driver"){
+                        document.getElementById("silent").style.backgroundColor = "blue"
+                    }
 
                     infoArea.innerHTML = ""
                 })
