f9115bfecd7ef32053c13d323c917f109b80741f
Michael Kofron <kofronmichael@gmail.com>
2020-10-11 12:27:00 -0500

    select options

--- selector.js
+++ selector.js
@@ -1,9 +1,14 @@
 let pinLocations = document.getElementById("pin-location-dropdown")
+let pinDefault = document.createElement("option")
+pinDefault.text = "Select Pin Location"
 
-for (let i = 1; i < 101; i++){
+for (let i = 1; i < 50; i++){
     let option = document.createElement("option")
     option.text = i
     option.value = i
 
     pinLocations.add(option)
 }
+
+let courses = document.getElementById("courses-dropdown")
+
